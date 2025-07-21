00c5d1a18160e1eb78424c38f5ab0b0d4983bb37
Nizamudheen C <nisunisam2@gmail.com>
2025-07-21 10:18:37 +0530

    Time: 81 ms (36.46%), Space: 66 MB (66.67%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+    let ans=''
+    for(let i=0;i<s.length;i++){
+      if(!(s[i]==s[i+1] && s[i]==s[i+2])){
+          ans+=s[i]
+        }
+    }
+    return ans
+
+};
