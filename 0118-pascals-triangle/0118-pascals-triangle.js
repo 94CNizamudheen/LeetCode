17cd485494f69491a6dbbdee819756507b5a311b
Nizamudheen C <nisunisam2@gmail.com>
2025-08-01 07:36:09 +0530

    Time: 0 ms (100%), Space: 53.4 MB (94.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,13 +3,13 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    let triangle=[];
+    let ans=[]
     for(let i=0;i<numRows;i++){
-        let row=new Array(i+1).fill(1);
-        for(let j=1;j<i;j++){
-            row[j]=triangle[i-1][j-1]+triangle[i-1][j];
-        }
-        triangle.push(row);
-    }  
-    return triangle;
+        let row=(new Array(i+1).fill(1))
+       for(let j=1;j<i;j++){
+            row[j]= ans[i-1][j-1]+ ans[i-1][j];
+       }
+       ans.push(row)
+    }
+    return ans
 };
