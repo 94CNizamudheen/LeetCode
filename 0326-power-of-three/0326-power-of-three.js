7c9e594fc67fb1085cc17a8a96e36c20fb4b8db7
Nizamudheen C <nisunisam2@gmail.com>
2025-08-13 08:07:37 +0530

    Time: 7 ms (49.56%), Space: 65 MB (71.86%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if(n<=0)return false;
-    while(n%3===0){
+     if(n<=0) return false;
+     while(n%3==0){
         n=n/3;
-    }
-    return (n===1) 
+     }
+     return n===1
 };
