c42f1cc361f08a5079e08057a6d0dbc26260f406
Nizamudheen C <nisunisam2@gmail.com>
2025-08-19 08:40:36 +0530

    Time: 3 ms (94.74%), Space: 67.5 MB (31.58%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var zeroFilledSubarray = function(nums) {
+
+    let count=0;
+    let streak=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===0){
+         streak++;
+         count+=streak;   
+        }else{
+            streak=0;
+        }
+    } 
+    return count
+};
