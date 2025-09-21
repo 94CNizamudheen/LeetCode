0f7918f82e7c00d5c820d05a2ef678b12e686940
Nizamudheen C <nisunisam2@gmail.com>
2025-09-21 15:47:18 +0530

    Time: 0 ms (100%), Space: 58.7 MB (8.59%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.js
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.js
@@ -5,17 +5,7 @@
 var countSubarrays = function(nums) {
     let count=0;
 
-    // for(let i=0;i<nums.length;i++){
-    //     let arr=[];
-    //     for(let j=i;j<i+3;j++){
-    //         if(j>nums.length) break
-    //         arr.push(nums[j]);
-    //         if(arr.length===3 && (arr[0]+arr[2])===(arr[1]/2)){
-    //             count++;
-    //         }
-    //     }
-    // }
-    // return count
+
     for(let i=0;i<nums.length-2;i++){
         if((nums[i]+nums[i+2])*2===nums[i+1]){
             count++;
